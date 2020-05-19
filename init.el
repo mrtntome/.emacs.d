b8ce80c6906fa55f974896674dc77e7a694cc6a3
mrtn.tome <mrtn.tome@gmail.com>
2020-05-18 21:29:16 -0300

    add auxtex & org-ref

--- init.el
+++ init.el
@@ -74,3 +74,30 @@
 (use-package org-bullets
   :ensure t
   :hook (org-mode . org-bullets-mode))
+
+(use-package auctex
+  :ensure t
+  :defer t)
+
+(use-package org-ref
+  :ensure t
+  :config
+  (setq reftex-default-bibliography '("~/Documents/biblio/refs.bib"))
+  (setq org-ref-bibliography-notes "~/Documents/biblio/notes.org"
+	org-ref-default-bibliography '("~/Documents/biblio/refs.bib")
+	org-ref-pdf-directory "~/Documents/biblio/pdfs/"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (auctex org-ref which-key use-package try spacemacs-theme spaceline pdf-tools org-bullets nlinum julia-mode exec-path-from-shell anaconda-mode ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
